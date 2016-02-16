c61ecf0bc9adb3a12b66ffd7492650b142a68846
oliver-jk-redding <oliver.jk.redding@gmail.com>
2016-02-17 09:01:14 +1300

    updates

--- js/nzhl.js
+++ js/nzhl.js
@@ -1,12 +1,12 @@
-setPageWrapperHeight();
+setPageWrapperHeight()
 
 function setPageWrapperHeight() {
-  var pageWrapper = document.querySelector(".page.wrapper");
+  var pageWrapper = document.querySelector(".upper-page.wrapper");
   var heroHeight = document.querySelector(".carousel").clientHeight;  
   pageWrapper.style.height = heroHeight + "px";
 }
 window.addEventListener("resize", setPageWrapperHeight);
-  
+
 var imgWrapper = document.querySelector(".hero-carousel");
 var imgNum = Math.floor((Math.random() * 9));
-imgWrapper.innerHTML = "<img src=\"images/carousel-pics/carousel-" + imgNum + ".jpg\" alt=\"NZHL Hobby\" class=\"carousel\">";
+imgWrapper.innerHTML = "<img src=\"images/carousel-pics/carousel-" + imgNum + ".jpg\" alt=\"NZHL Hobby\" width=\"\" class=\"carousel\">";
