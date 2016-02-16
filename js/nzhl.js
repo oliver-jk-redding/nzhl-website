2636700576659c2b92f0b3eb4b2f9f6072b0b625
oliver-jk-redding <oliver.jk.redding@gmail.com>
2016-02-16 23:49:06 +1300

    add reloading background image

--- js/nzhl.js
+++ js/nzhl.js
@@ -2,8 +2,11 @@ setPageWrapperHeight();
 
 function setPageWrapperHeight() {
   var pageWrapper = document.querySelector(".page.wrapper");
-  var heroHeight = document.querySelector(".hero.wrapper").clientHeight;  
+  var heroHeight = document.querySelector(".carousel").clientHeight;  
   pageWrapper.style.height = heroHeight + "px";
 }
-
 window.addEventListener("resize", setPageWrapperHeight);
+  
+var imgWrapper = document.querySelector(".hero-carousel");
+var imgNum = Math.floor((Math.random() * 9));
+imgWrapper.innerHTML = "<img src=\"images/carousel-pics/carousel-" + imgNum + ".jpg\" alt=\"NZHL Hobby\" class=\"carousel\">";
